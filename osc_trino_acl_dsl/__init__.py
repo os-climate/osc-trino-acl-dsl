99712d0f587cb9a396fddf1ae56e9779161cb137
Erik Erlandson <eerlands@redhat.com>
2022-01-19 11:33:49 -0700

    add setup.py

---
+++ osc_trino_acl_dsl/__init__.py
@@ -0,0 +1,15 @@
+"""
+osc-trino-acl-dsl
+
+A DSL for generating rules.json files for Trino
+"""
+
+# defines the release version for this python package
+__version__ = "0.1.0"
+
+from .dsl2rules import *
+
+__all__ = [
+    "dsl_to_rules",
+]
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+import pathlib
+
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).resolve().parent
+
+# The text of the README file is used as a description
+README = (HERE / "README.md").read_text()
+
+setup(
+    name = "osc-trino-acl-dsl",
+    version = "0.1.0",
+    description = "A DSL for generating rules.json files for Trino",
+    long_description = README,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/os-climate/osc-trino-acl-dsl",
+    author = "OS-Climate",
+    author_email = "eje@redhat.com",
+    license = "Apache-2.0",
+    classifiers = [
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = [],
+    entry_points = {
+        "console_scripts": [
+            "trino-dsl-to-rules=osc_trino_acl_dsl.__main__:main",
+        ],
+    },
+)
+
