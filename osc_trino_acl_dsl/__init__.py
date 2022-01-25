a9dda3d875b0f68ff70c9128bed5cb2adfec7e9c
Erik Erlandson <eerlands@redhat.com>
2022-01-25 15:00:01 -0700

    bump to 0.2.0

--- osc_trino_acl_dsl/__init__.py
+++ osc_trino_acl_dsl/__init__.py
@@ -5,7 +5,7 @@ A DSL for generating rules.json files for Trino
 """
 
 # defines the release version for this python package
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .dsl2rules import *
 
