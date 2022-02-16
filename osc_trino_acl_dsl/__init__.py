3cf434d965312b6be7e317e3a9cc28b490b10776
Erik Erlandson <eerlands@redhat.com>
2022-02-15 18:04:43 -0700

    release 0.2.4

--- osc_trino_acl_dsl/__init__.py
+++ osc_trino_acl_dsl/__init__.py
@@ -5,7 +5,7 @@ A DSL for generating rules.json files for Trino
 """
 
 # defines the release version for this python package
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 from .dsl2rules import *
 
