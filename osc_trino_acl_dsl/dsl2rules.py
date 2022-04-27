fb25a5424d3ad9f4b5f23af5306d71e8b5235edb
Erik Erlandson <eerlands@redhat.com>
2022-04-27 05:31:06 -0700

    pay the type checking troll
    Signed-off-by: Erik Erlandson <eerlands@redhat.com>

--- osc_trino_acl_dsl/dsl2rules.py
+++ osc_trino_acl_dsl/dsl2rules.py
@@ -105,7 +105,7 @@ def dsl_to_rules(dsl: dict, validate=True) -> dict:  # noqa: C901
     # in the catalog section
     # https://trino.io/docs/current/security/file-system-access-control.html#catalog-schema-and-table-access
     # note there is not a similar issue for table -> schema ownerships
-    uallow = {}
+    uallow: dict = {}
 
     # table rules go here
     for spec in dsl["tables"]:
@@ -184,10 +184,10 @@ def dsl_to_rules(dsl: dict, validate=True) -> dict:  # noqa: C901
         # configure group(s) with read+write access to this catalog
         # I have concerns about how using "|" style regex is going to scale if number
         # of schemas and tables grows large, so I am going to encode these as individual rules
-        ugs = set([e["group"] for e in uallow.get(spec["catalog"], []) if "group" in e])
+        ugs = list(set([e["group"] for e in uallow.get(spec["catalog"], []) if "group" in e]))
         for ug in ugs:
             catalog_rules.append(_union({"group": ug}, rule))
-        ugs = set([e["user"] for e in uallow.get(spec["catalog"], []) if "user" in e])
+        ugs = list(set([e["user"] for e in uallow.get(spec["catalog"], []) if "user" in e]))
         for ug in ugs:
             catalog_rules.append(_union({"user": ug}, rule))
 
